219db0153d9ef5176522d001d2447724727e564b
teejayk <t.kongolo@alustudent.com>
2025-10-26 14:02:01 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -9,11 +9,24 @@ import requests
 
 
 def top_ten(subreddit):
-    """Top ten subreddit"""
+    """
+    Queries the Reddit API and prints the titles of the first 10 hot posts
+    for a given subreddit.
+
+    Args:
+        subreddit: The name of the subreddit to query
+
+    Returns:
+        None
+    """
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {'User-Agent': 'linux:reddit.api.project:v1.0 (by /u/yourusername)'}
+    headers = {
+        'User-Agent': 'linux:reddit.api.project:v1.0 (by /u/yourusername)'
+    }
+
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
+        response = requests.get(url, headers=headers,
+                                allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
             posts = data.get('data').get('children')
